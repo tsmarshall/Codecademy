055a2c506bfe94f3283ff95360ffa465d49ef816
Thomas Marshall <tsmarshall24@gmail.com>
2014-12-11 21:58:03 +0930

    ex 9 added

---
+++ ex9conditions.py
@@ -0,0 +1,17 @@
+#Evaluate expressions
+bool_one = False or not True and True
+
+bool_two = False and not True or True
+
+bool_three = True and not (False or False) 
+
+bool_four = not not True or False and not True
+
+bool_five = False or not (True and True)
+
+#Print Outputs
+print bool_one 
+print bool_two
+print bool_three
+print bool_four
+print bool_five
